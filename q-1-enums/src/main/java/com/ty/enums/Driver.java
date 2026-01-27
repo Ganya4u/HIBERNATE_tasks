e8b3d2a9a63eb6114875d980d4b326fdd7487e60
Rahul Khandekar <khandekar0000021@gmail.com>
2026-01-27 11:52:58 +0530

    Done.

---
+++ q-1-enums/src/main/java/com/ty/enums/Driver.java
@@ -0,0 +1,22 @@
+package com.ty.enums;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Emp e =new Emp("Rahul", 24, Role.ADMIN );
+		e.display();   
+		
+		
+		System.out.println(Role.ADMIN);  //ADMIN
+		
+		Role r = Role.USER;
+
+		System.out.println(r);  //USER
+
+		
+				
+
+	}
+
+}
